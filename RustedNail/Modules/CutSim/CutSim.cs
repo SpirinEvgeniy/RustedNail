d48d9cb3313a39ac3ffbe81d4c06f6e76ff14e30
Nikolay Makalov <nexterrible@mail.ru>
2021-10-18 15:23:38 +0700

    Реализовал закрытие модулей, отрефакторил CutSim под шаблон, создал FAQ с планом добавления модулей

---
+++ RustedNail/Modules/CutSim/CutSim.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RustedNail.Modules.CutSim
+{
+    public class CutSim : ProgramModule
+    {
+        //здесь создайте поля для ваших форм, с модификатором доступа private
+        private CutSim_MainForm mainForm;
+
+        public void Load(ParentMDIForm parentMDIForm)
+        {
+            parentForm = parentMDIForm;
+            //здесь пишите ваш доп. код для подгрузки (инициализация форм, их заголовок и т.п.)
+            //Обязательно каждую форму добавьте в коллекцию форм formCollection!
+            mainForm = new CutSim_MainForm();
+            mainForm.Text = "Cut Sim";
+            mainForm.MdiParent = parentForm;
+            mainForm.Width = 600;
+            mainForm.Height = 400;
+            mainForm.Show();
+
+            formCollection.Add(mainForm);
+            ChangeToolStripSettings(parentMDIForm);
+        }
+
+        private void ChangeToolStripSettings(ParentMDIForm parentMDIForm)
+        {
+            InitialiseToolStrip(parentMDIForm);
+
+            //здесь пишите ваш код для настройки ToolStrip (добавление кнопок, подгрузка иконок и т.п.)
+            //первым ToolStripItem'ом обязательно должен быть Label с названием вашего модуля
+            //Каждый созданный item добавьте в toolStip
+
+            ToolStripLabel toolStripItem1Label = new ToolStripLabel();
+            toolStripItem1Label.Text = "Cut Sim";
+
+            toolStrip.Items.Add(toolStripItem1Label);
+        }
+    }
+}
---
+++ RustedNail/Modules/CutSim/CutSim_MainForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RustedNail.Modules.CutSim
+{
+    public partial class CutSim_MainForm : Form
+    {
+        private TabControl tabControl;
+        private TabPage tabPage1;
+        private PictureBox pictureBox;
+
+        public CutSim_MainForm()
+        {
+            Text = "CutSim";
+            CutSimTabs();
+        }
+
+        private void CutSimTabs()
+        {
+            this.tabControl = new TabControl();
+            this.tabPage1 = new TabPage();
+
+            this.tabControl.Multiline = true;
+            this.tabControl.Controls.AddRange(new Control[] { this.tabPage1 });
+            this.tabControl.Size = new Size(450, 0);
+
+
+            this.tabPage1.Text = "Парметры";
+            this.Controls.AddRange(new Control[] { this.tabControl });
+            this.tabControl.Dock = DockStyle.Right;
+
+
+            this.pictureBox = new PictureBox();
+            this.pictureBox.Parent = this.tabPage1;
+            this.pictureBox.Dock = DockStyle.Top;
+            this.pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.pictureBox.ClientSize = new Size(450, 250);
+            this.pictureBox.Image = Image.FromFile(@"..\..\RustedNail\Modules\CutSim\Исходный контур.png");
+        }
+    }
+}
